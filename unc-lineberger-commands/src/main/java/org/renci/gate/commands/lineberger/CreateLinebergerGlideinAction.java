5e6a92de399a42de3aa2888be07c071a5bde73ee
Jason Reilly <jdr0887@gmail.com>
2014-10-03 09:08:40 -0400

    changing name of job

--- unc-lineberger-commands/src/main/java/org/renci/gate/commands/lineberger/CreateLinebergerGlideinAction.java
+++ unc-lineberger-commands/src/main/java/org/renci/gate/commands/lineberger/CreateLinebergerGlideinAction.java
@@ -49,7 +49,7 @@ public class CreateLinebergerGlideinAction extends AbstractAction {
             callable.setSite(site);
             callable.setCollectorHost(collectorHost);
             callable.setUsername(System.getProperty("user.name"));
-            callable.setJobName("glidein");
+            callable.setJobName(String.format("glidein-%s", site.getName().toLowerCase()));
             callable.setQueue(queue);
             callable.setSubmitDir(submitDir);
             callable.setRequiredMemory(40);
